3978c09dfd89cfe37da5c47f2556e5b37ef43f6d
Gonzalo.D <gonzaloagustindelgado@gmail.com>
2024-10-07 17:34:50 -0300

    Arreglo de tipado

--- src/components/RecommendedAlbum.tsx
+++ src/components/RecommendedAlbum.tsx
@@ -3,39 +3,6 @@ import Card from "./Card";
 import RecommendedAlbumStyle from "../styles/RecommendedAlbumStyle.module.css";
 import useFetchData from "../customhooks/useFetchData";
 
-const ALBUMS = [
-    {
-        title: "Igor",
-        info: "Álbum • Tyler, The Creator",
-        img: "src/assets/img/igor.jfif",
-        id: "Igor",
-    },
-    {
-        title: "Siempre Es Hoy",
-        info: "Álbum • Gustavo Cerati",
-        img: "src/assets/img/siempreeshoy.jfif",
-        id: "Siempre-Es-Hoy",
-    },
-    {
-        title: "Tinchera",
-        info: "Álbum • Babasonicos",
-        img: "src/assets/img/trinchera.png",
-        id: "Tinchera",
-    },
-    {
-        title: "Tu Duo Favorito",
-        info: "Álbum • YSY A, Bhavi",
-        img: "src/assets/img/tuduofav.jfif",
-        id: "Tu-Duo-Favorito",
-    },
-    {
-        title: "Operation: Doomsday",
-        info: "Álbum • MF DOOM",
-        img: "src/assets/img/doomsday.jfif",
-        id: "Operation-Doomsday",
-    },
-];
-
 type Props = {
     classname?: string;
 };
